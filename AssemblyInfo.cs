21c049baa26a26aacc3d8fa9053e2f0db873a659
Cutie Lewko <45269833+Lewko6702@users.noreply.github.com>
2022-09-05 04:16:37 +0100

    Added Updated Files for Update
    Still broken trying to find what to replace Storage with

---
+++ AssemblyInfo.cs
@@ -0,0 +1,2 @@
+﻿using MelonLoader;
+[assembly: MelonInfo(typeof(VeryPoorQuest.VeryPoorQuestMod), "2.0.0", "Emilia")]
--- VeryPoorQuest.cs
+++ VeryPoorQuest.cs
@@ -2,25 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using VRCModLoader;
+using MelonLoader;
 
 namespace VeryPoorQuest
 {
-    [VRCModInfo("VeryPoorQuest", "1.0.0", "Emilia")]
-    public class VeryPoorQuest : VRCMod
+    public class VeryPoorQuestMod
     {
         static void OnApplicationStart()
         {
             if (UnityEngine.Application.platform == UnityEngine.RuntimePlatform.WindowsPlayer)
-                VRCModLogger.Log("[VeryPoorQuest] This module is intended for use on the Quest! PC users can already set their performance rating minimum...");
+                MelonLogger.Msg("[VeryPoorQuest] This module is intended for use on the Quest! PC users can already set their performance rating minimum...");
         }
         void OnLevelWasLoaded(int level)
         {
-            if (level == 1 && (int)Storage.Read("VRC_AVATAR_PERFORMANCE_RATING_MINIMUM_TO_DISPLAY", typeof(int), 5) != 5)
+            if (level == 1 && (int)VRC_DataStorageInternal.Read("VRC_AVATAR_PERFORMANCE_RATING_MINIMUM_TO_DISPLAY", typeof(int), 5) != 5)
             {
-                Storage.Write("VRC_AVATAR_PERFORMANCE_RATING_MINIMUM_TO_DISPLAY", 5);
-                Storage.SaveNow();
-                VRCModLogger.Log("[VeryPoorQuest] The deed is done. Have a nice day!");
+                VRC_DataStorageInternal.Write("VRC_AVATAR_PERFORMANCE_RATING_MINIMUM_TO_DISPLAY", 5);
+                VRC_DataStorageInternal.SaveNow();
+                MelonLogger.Msg("[VeryPoorQuest] The deed is done. Have a nice day!");
             }
         }
     }
